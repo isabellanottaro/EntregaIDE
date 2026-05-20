6de5adef79fe226008e8a33ca60efa8c5204d0f0
IsabellaNottaro <isabellanottaro@gmail.com>
2026-05-20 18:48:29 +0200

    cambios

--- src/clases/Pedido.java
+++ src/clases/Pedido.java
@@ -1,9 +1,24 @@
 package clases;
 
+import java.time.LocalDate;
+
 public class Pedido {
 	private int id;
 	private LocalDate fechaDePedido;
 	private EstadoPedido estado;
+	private Cliente cliente;
+	private Repartidor repartidor;
+	private List<LineaPedido> lineasPedido;
+	
+	public Pedido (int id, Cliente cliente) {
+		this.id=id;
+		this.cliente=cliente;
+		this.fechaDePedido=LocalDate.now();
+		this.estado=EstadoPedido.PENDIENTE;
+		this.lineasPedido=new ArrayList<>();
+		this.repartidor=null;
+		
+	}
 	
 	
 }
