a4448d99b5db869e2d45b83e5facecfb92c93e99
IsabellaNottaro <isabellanottaro@gmail.com>
2026-05-20 10:40:42 +0200

    clase usuario hecha

---
+++ src/clases/Usuario.java
@@ -0,0 +1,37 @@
+package clases;
+
+public abstract class Usuario {
+	private String nombre;
+	private String email;
+	private String telefono;
+
+	public Usuario(String nombre, String email, String telefono) {
+		this.nombre = nombre;
+		this.email = email;
+		this.telefono = telefono;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail(String email) {
+		return email;
+	}
+
+	public void setEmail() {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module EentregaIDE_INS {
+}
