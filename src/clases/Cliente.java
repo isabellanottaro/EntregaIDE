5493c237e99cb32d8a01cefe4e8156025415d5f6
IsabellaNottaro <isabellanottaro@gmail.com>
2026-05-23 14:38:25 +0200

    Nueva clase repartidor y estados repartidor completados

--- src/clases/Cliente.java
+++ src/clases/Cliente.java
@@ -1,7 +1,11 @@
 package clases;
 
 public class Cliente extends Usuario{
-	
 	private List<Pedido> pedidos;
+	
+	public Cliente(String nombre, String email, String telefono) {
+		super(nombre, email, telefono);
+		this.pedidos=new ArrayList<>();	
+	}
 
 }
