92e1ca46be008073ffbdfcf55e9728fceb5c5bf0
IsabellaNottaro <isabellanottaro@gmail.com>
2026-05-23 19:33:40 +0200

    Comprobaciones en el main

--- src/main/Main.java
+++ src/main/Main.java
@@ -29,6 +29,45 @@ public class Main {
 		System.out.println("Repartidor disponible: " + repartidor.isDisponible());
 		
 		
+		System.out.println("\n PRUEBA PRODUCTO DUPLICADO");
+		Pedido pedido2 = new Pedido (2,cliente);
+		pedido2.addProducto(pizza, 1);
+		try {
+			pedido2.addProducto(pizza, 1);
+			System.out.println("ERROR: debería haber lanzado excepción");
+		} catch (IllegalArgumentException e) {
+			System.out.println("Correcto - " + e.getMessage());
+		}
+		
+		System.out.println("\n PRUEBA ASIGNAR SIN PRODUCTOS");
+        Pedido pedido3 = new Pedido(3, cliente);
+        try {
+            pedido3.asignarRepartidor(repartidor);
+            System.out.println("ERROR: debería haber lanzado excepción");
+        } catch (IllegalStateException e) {
+            System.out.println("Correcto - " + e.getMessage());
+        }
+        
+        System.out.println("\n PRUEBA ENTREGAR SIN ESTAR EN REPARTO ");
+        Pedido pedido4 = new Pedido(4, cliente);
+        pedido4.addProducto(refresco, 2);
+        try {
+            pedido4.entregar();
+            System.out.println("ERROR: debería haber lanzado excepción");
+        } catch (IllegalStateException e) {
+            System.out.println("Correcto - " + e.getMessage());
+        }
+        
+        System.out.println("\n PRUEBA CANCELAR PEDIDO ===");
+        Pedido pedido5 = new Pedido(5, cliente);
+        pedido5.addProducto(pizza, 1);
+        pedido5.asignarRepartidor(repartidor);
+        System.out.println("Estado antes de cancelar: " + pedido5.getEstado());
+        System.out.println("Repartidor ocupado: " + !repartidor.isDisponible());
+        pedido5.cancelar();
+        System.out.println("Estado tras cancelar: " + pedido5.getEstado());
+        System.out.println("Repartidor libre tras cancelar: " + repartidor.isDisponible());
+		
 		
 		
 		
